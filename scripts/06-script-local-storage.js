c6868cd6867e7611a53878db8caf4ab934540697
Matthew Wang <matwan@bergen.org>
2023-02-15 10:48:46 -0500

    merge local storage and class changes

--- scripts/06-script-local-storage.js
+++ scripts/06-script-local-storage.js
@@ -26,51 +26,69 @@ function updateBalance(amount, isNegative){
         coinAddSoundElm.play();
     }
     balanceAmountElm.textContent = balance.toFixed(2);
+
+    if (balance < 0) {
+        balanceElm.classList.add("negative");
+    } else {
+        balanceElm.classList.remove("negative");
+    }
+    
     //Use localStorage to save the updated balance
     localStorage.setItem("savedBalance", balance);
 }
 
-pennyElm.addEventListener("click", function (event) {
-    updateBalance(0.01, event.shiftKey);
-});
-
-nickelElm.addEventListener("click", function (event) {
-    updateBalance(0.05, event.shiftKey);
-});
-
-dimeElm.addEventListener("click", function (event) {
-    updateBalance(0.10, event.shiftKey);
-});
-
-quarterElm.addEventListener("click", function (event) {
-    updateBalance(0.25, event.shiftKey);
-});
-
-//event here is a KeyboardEvent object
 document.addEventListener("keydown", function (event) {
+    if (event.repeat) { 
+        return
+    }
+
     if(event.key === "p"){
+        pennyElm.classList.add("keypress");
         updateBalance(0.01, false);
     }
     if(event.key === "n"){
+        nickelElm.classList.add("keypress");
         updateBalance(0.05, false);
     }
     if(event.key === "d"){
+        dimeElm.classList.add("keypress");
         updateBalance(0.10, false);
     }
     if(event.key === "q"){
+        quarterElm.classList.add("keypress");
         updateBalance(0.25, false);
     }
     if(event.key === "P"){
+        pennyElm.classList.add("keypress");
         updateBalance(0.01, true);
     }
     if(event.key === "N"){
+        nickelElm.classList.add("keypress");
         updateBalance(0.05, true);
     }
     if(event.key === "D"){
+        dimeElm.classList.add("keypress");
         updateBalance(0.10, true);
     }
     if(event.key === "Q"){
+        quarterElm.classList.add("keypress");
         updateBalance(0.25, true);
+    }
+});
+
+document.addEventListener("keyup", function (event) {
+    // The || is an "OR" logical statement
+    if(event.key === "p" || event.key === "P"){
+        pennyElm.classList.remove("keypress");
+    }
+    if(event.key === "n" || event.key === "N"){
+        nickelElm.classList.remove("keypress");
+    }
+    if(event.key === "d" || event.key === "D"){
+        dimeElm.classList.remove("keypress");
+    }
+    if(event.key === "q" || event.key === "Q"){
+        quarterElm.classList.remove("keypress");
     }
 });
 
